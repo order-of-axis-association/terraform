4de7e5724e327a1bc332abfb1287c01e9d4d52d9
Yuto Takamoto <scarletjaeger@gmail.com>
2019-07-28 04:48:45 +0000

    Remove ssh key from secret and instead use kms encrypted ciphertext

--- workspaces/OA-shared/repos.tf
+++ workspaces/OA-shared/repos.tf
@@ -11,7 +11,10 @@ resource "google_cloudbuild_trigger" "docker-fluentd-deploy-trigger" {
   }
 
   substitutions = {
-    _AQUABOT_SECRETS_PEM = "${file("secrets/aquabot-secrets-key")}"
+    REGION = "${var.region}"
+    KMS_KEY_RING_NAME = "${var.kms_key_ring_name}"
+    AQUABOT_SECRET_KMS_CRYPTO_KEY = "${var.aquabot_secret_kms_crypto_key}"
+    AQUABOT_SECRETS_PEM_ENC_BASE64 = "${file("secrets/aquabot_pem_kms_ciphertext_base64.enc")}"
   }
 
   filename = "cloudbuild.yml"
