c8aa525d2b804fe8a435fefbde3398429edd4640
Yuto Takamoto <scarletjaeger@gmail.com>
2019-07-28 03:17:43 +0000

    Add aquabot-secrets-key from secrets submodule in cloudbuild

--- workspaces/OA-shared/repos.tf
+++ workspaces/OA-shared/repos.tf
@@ -10,5 +10,9 @@ resource "google_cloudbuild_trigger" "docker-fluentd-deploy-trigger" {
     repo_name   = "${google_sourcerepo_repository.docker-fluentd.name}"
   }
 
+  substitutions = {
+    _AQUABOT_SECRETS_PEM = "${file("secrets/aquabot-secrets-key")}"
+  }
+
   filename = "cloudbuild.yml"
 }
